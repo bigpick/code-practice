1ca82a8ed10496f24f7d2d7c9ba3f9896338a913
George Pickering <9803299+bigpick@users.noreply.github.com>
2024-08-26 12:43:09 -0400

    chore: add leetcode 0169

--- leetcode/0080_remove_duplicates_from_sorted_array_2/solution.py
+++ leetcode/0080_remove_duplicates_from_sorted_array_2/solution.py
@@ -1,29 +1,40 @@
 #!/usr/bin/env python
 
+# Given an integer array nums sorted in non-decreasing order, remove some duplicates
+# in-place such that each unique element appears at most twice. The relative order of
+# the elements should be kept the same.
+#
+# Since it is impossible to change the length of the array in some languages, you
+# must instead have the result be placed in the first part of the array nums. More
+# formally, if there are k elements after removing the duplicates, then the first k
+# elements of nums should hold the final result. It does not matter what you leave
+# beyond the first k elements.
+#
+# Return k after placing the final result in the first k slots of nums
+#
+# Do not allocate extra space for another array. You must do this by modifying the
+# input array in-place with O(1) extra memory.
+
 TESTS_INPUT = [
-    ([1, 1, 2], 2, [1, 2]),
-    ([0, 0, 1, 1, 1, 2, 2, 3, 3, 4], 5, [0, 1, 2, 3, 4]),
+    ([1, 1, 1, 2, 2, 3], 5, [1, 1, 2, 2, 3, -1]),
+    ([0, 0, 1, 1, 1, 1, 2, 3, 3], 7, [0, 0, 1, 1, 2, 3, 3, -1, -1]),
 ]
 
 
 class Solution:
     def removeDuplicates(self, nums: list[int]) -> int:
-        # Given an integer array nums sorted in non-decreasing order, remove the
-        # duplicates in-place such that each unique element appears only once. The
-        # relative order of the elements should be kept the same. Then return the
-        # number of unique elements in nums.
-        last_seen = None
-        idx = len(nums) - 1
+        idx = 0
 
-        while idx >= 0:
-            current = nums[idx]
-            if current == last_seen:
-                nums.pop(idx)
-            idx -= 1
-            last_seen = current
-        return len(nums)
+        for item in nums:
+            if idx < 2 or item != nums[idx - 2]:
+                nums[idx] = item
+                idx += 1
+
+        return idx
 
 
 for test_case in TESTS_INPUT:
-    assert Solution().removeDuplicates(test_case[0]) == test_case[1]
-    assert test_case[0] == test_case[2]
+    ans = Solution().removeDuplicates(test_case[0])
+    print(ans)
+    assert ans == test_case[1]
+    assert test_case[0][: test_case[1]] == test_case[2][: test_case[1]]
---
+++ leetcode/0169_majority_element/solution.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+
+# Given an array `nums` of size `n`, return the _majority element_.
+#
+# The _majority element_ is the element that appears more than [n / 2] times. You
+# may assume that the majority element _always_ exists in the array.
+#
+#  Follow-up: Could you solve the problem in linear time and in O(1) space?
+
+TEST_CASES = [([3, 2, 3], 3), ([2, 2, 1, 1, 1, 2, 2], 2)]
+
+
+class Solution:
+    def naive_majorityElement(self, nums: list[int]) -> int:
+        # O(n^2)
+        min_occurence = len(nums) // 2
+        d = {}
+        for elem in nums:
+            try:
+                d[elem] += 1
+            except KeyError:
+                d[elem] = 1
+
+        for k, v in d.items():
+            if v > min_occurence:
+                return k
+
+    def majorityElement(self, nums: list[int]) -> int:
+        current_item = None
+        current_count = None
+        for num in nums:
+            if current_item is None:
+                current_item = num
+                current_count = 1
+                continue
+
+            if num != current_item:
+                current_count -= 1
+
+            if current_count == 0:
+                current_item = num
+                current_count = 1
+
+        return current_item
+
+
+for test_case in TEST_CASES:
+    print(Solution().naive_majorityElement(test_case[0]))
+    assert Solution().naive_majorityElement(test_case[0]) == test_case[1]
+
+    print(Solution().majorityElement(test_case[0]))
+    assert Solution().majorityElement(test_case[0]) == test_case[1]

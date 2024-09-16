77a3e326befddc234fa34763ffaa6777fb2f5f57
George Pickering <9803299+bigpick@users.noreply.github.com>
2024-09-16 13:50:14 -0400

    fix: leetcode stub for ruff formatting

--- leetcode/stub.py
+++ leetcode/stub.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 
-TEST_CASES=[]
+TEST_CASES = []
 
 
 class Solution:
+    def replace(self):
+        pass
 
 
 for test_case in TEST_CASES:
-    assert Solution(). ==
+    assert Solution().replace() == None

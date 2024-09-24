f989a6c06884a31fdc15974e25cf10cb01fc64b1
George Pickering <9803299+bigpick@users.noreply.github.com>
2024-09-24 08:26:02 -0400

    feat: leetcode 2828 string is acronym

---
+++ leetcode/2828_string_is_acronym/solution.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+# Given an array of strings `words` and a string `s`, determin if `s` is an **acronym**
+# of words.
+#
+# The string `s` ins considered ot be an acronym of `words` if it can be formed by
+# concatenating the **first** character of each string in `words` in order.
+#
+# e.g. "ab" can be formed from ["apple", "banana"], but not from ["bear", "aardvark"]
+#
+# Return true if so, false otherwise
+
+TEST_CASES = [
+    (["alice", "bob", "charlie"], "abc", True),
+    (["an", "apple"], "a", False),
+    (["never", "gonna", "give", "up", "on", "you"], "ngguoy", True),
+]
+
+
+class Solution:
+    def isAcronym(self, words: list[str], s: str) -> bool:
+        if len(words) != len(s):
+            return False
+        for idx, val in enumerate(words):
+            if s[idx] != val[0]:
+                return False
+
+        return True
+
+
+for test_case in TEST_CASES:
+    assert Solution().isAcronym(test_case[0], test_case[1]) == test_case[2]

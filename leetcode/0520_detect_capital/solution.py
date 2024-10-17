26c8db083c87d8e89e395a46302d7c05c0171535
George Pickering <9803299+bigpick@users.noreply.github.com>
2024-10-17 16:19:28 -0400

    feet: add letcode 520

---
+++ leetcode/0520_detect_capital/solution.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+TEST_CASES = [("USA", True), ("FlaG", False), ("leetcode", True), ("lfAG", False)]
+
+
+class Solution:
+    def detectCapitalUse(self, word: str) -> bool:
+        from string import ascii_uppercase
+
+        cap = 0
+        for c in word:
+            if c in ascii_uppercase:
+                cap += 1
+
+        return cap == 0 or cap == len(word) or cap == 1 or False
+
+
+for test_case in TEST_CASES:
+    assert Solution().detectCapitalUse(test_case[0]) == test_case[1]
